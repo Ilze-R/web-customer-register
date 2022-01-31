d512375fb0089c8fa89033034247bf8b6e57aed5
Ilze Rigerte <irigerte@gmail.com>
2022-01-31 19:09:58 +0200

    update/delete options

--- src/main/java/com/example/webcustomerregister/CustomerController.java
+++ src/main/java/com/example/webcustomerregister/CustomerController.java
@@ -2,10 +2,11 @@ package com.example.webcustomerregister;
 
 import com.example.webcustomerregister.dao.CustomerDAO;
 import com.example.webcustomerregister.entity.Customer;
+import com.example.webcustomerregister.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,15 +14,59 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @Autowired
-    private CustomerDAO customerDAO;
+   // @Autowired
+   // private CustomerDAO customerDAO;
+    //removing because of creating customer service
 
-    @RequestMapping("/list")
-    public String listCustomers(Model theModel){
-        List<Customer> theCustomers = customerDAO.getCustomers();
+    @Autowired
+    private CustomerService customerService;
+
+    // @RequestMapping("/list")
+    @GetMapping("/list")
+    public String listCustomers(Model theModel) {
+        List<Customer> theCustomers = customerService.getCustomers();
 
         theModel.addAttribute("customers", theCustomers);
 
         return "list-customers";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormFromAdd(Model theModel){
+
+        //model attribute to bind form data
+        Customer theCustomer = new Customer();
+        theModel.addAttribute("customer", theCustomer);
+
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer){
+
+       //save the customer using service
+        customerService.saveCustomer(theCustomer);
+
+        return "redirect:/customer/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel){
+
+        //getting customer from service
+        Customer theCustomer = customerService.getCustomer(theId);
+
+        //setting customer as a model attribute to pre-populate the form
+        theModel.addAttribute("customer", theCustomer);
+
+        return "customer-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCustomer(@RequestParam("customerId") int theId){
+
+        customerService.deleteCustomer(theId);
+
+        return "redirect:/customer/list";
     }
 }
